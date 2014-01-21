9eb1a4e5d9a7fd7914c33f29cbd84e4284cce63b
Jordan-Ray <jordan.b.ray40@gmail.com>
2014-01-21 08:03:06 -0700

    Dont't View Ever

---
+++ src/simple/contoller/StupidRunner.java
@@ -0,0 +1,15 @@
+package simple.contoller;
+
+public class StupidRunner
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
